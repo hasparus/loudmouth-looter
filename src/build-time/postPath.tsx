5bcad884c86d1c1c244ea52d1bf907fe35ab24a5
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-06-24 22:49:55 +0200

    postPath: map folder index.mdx posts to folder route

--- src/build-time/postPath.tsx
+++ src/build-time/postPath.tsx
@@ -1,11 +1,12 @@
 import { relative } from "node:path";
 
 export function postPath(postsDir: string, filePath: string): string {
-  return (
-    "/" +
-    relative(postsDir, filePath)
-      .replace(/\\/g, "/")
-      .replace(/\.mdx?$/, "")
-      .replace(/ /g, "-")
-  );
+  const slug = relative(postsDir, filePath)
+    .replace(/\\/g, "/")
+    .replace(/\.mdx?$/, "")
+    .replace(/\/index$/, "")
+    .replace(/^index$/, "")
+    .replace(/ /g, "-");
+
+  return "/" + slug;
 }
